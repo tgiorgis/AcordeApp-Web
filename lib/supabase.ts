ad8ef6cdd5572b1fc3a9f0504620d30544ecacc0
tgiorgis <tatigiorgis@gmail.com>
2026-03-30 12:35:05 +1100

    fix: despliegue web exitoso con index html

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native'; // <--- Agregamos esto
 
-// PEGA TUS DATOS AQUÍ DIRECTAMENTE
 const supabaseUrl = 'https://diapmgoiolxbmqkhqlbq.supabase.co'; 
 const supabaseAnonKey = 'sb_publishable_JrOdMyxehdfEFlk5DNr2jQ_ZTId6cVc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    // Si es web, no usamos AsyncStorage para que no tire error
+    storage: Platform.OS === 'web' ? undefined : AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
